bc4fb71e6004d62e868dd6de1c63917dc6fb6fc1
FinnCummins <finnkcummins@gmail.com>
2023-07-01 13:23:52 +0100

    Added new fonts to the about and services pages

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,17 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'title-color': '#654321',
+        		'body-color': '#333333',
+        		'cta-color': '#006400',
+			},
+			fontFamily: {
+				'serif': ['Merriweather', 'serif'],
+				'sans': ['Roboto', 'sans-serif'],
+			},
+		},
 	},
 	plugins: [],
 }
